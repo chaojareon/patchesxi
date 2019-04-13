b883f3cacc9ac9698d79d57d143a2554c5173017
Theo Chaojareon <theo@chaojareon.com>
2019-04-12 23:05:09 -0400

    Changed esxcli methodology, updated README.md, and added .gitignore

---
+++ README.md
@@ -0,0 +1,11 @@
+# patchesxi
+Powershell/Powercli script to update ESXi from the cli without having to enable ssh on ESXi.
+
+## Usage
+1. Edit the patchesxi.ps1 file and enter the ESXi host and the profile dept url.
+2. Run `$ pwsh patchesxi.ps1`
+
+
+### Note
+This was tested on Powershell 6 core on Ubuntu 18.04 LTS with PowerCLI 11.2.
+
--- patchesxi.ps1
+++ patchesxi.ps1
@@ -1,16 +1,15 @@
 # patchesxi.ps1
 # PS script to run on single free esxi host patch update and to reboot
 
-$viserver = "[host name or IP]"
-$profiledepot = "[url to depot]"
-$profile = "[profile name]"
+$viserver = ""
+$profiledepot = ""
 
 Import-Module VMware.PowerCLI
 Connect-VIServer $viserver
 $esxcli = Get-EsxCli -V2
-$patchargs = $esxcli.software.profile.update.createargs()
+$patchargs = $esxcli.software.vib.install.createargs()
 $patchargs.depot = $profiledepot
-$patchargs.profile = $profiledepot
-$esxcli.software.profile.update.Invoke($patchargs)
+$esxcli.software.vib.install.Invoke($patchargs)
 $rebootargs = $esxcli.system.shutdown.reboot.CreateArgs()
+$rebootargs.reason = "Patching"
 $esxcli.system.shutdown.reboot.Invoke($rebootargs)
