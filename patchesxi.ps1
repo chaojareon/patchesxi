a25f8fec21003354f2f0fa44b2bcfbfed5d33b08
Theo Chaojareon <theo@chaojareon.com>
2019-08-04 00:30:06 -0400

    added parameters, fixed how it updates

--- patchesxi.ps1
+++ patchesxi.ps1
@@ -1,15 +1,28 @@
 # patchesxi.ps1
 # PS script to run on single free esxi host patch update and to reboot
+# Info for patching in this manner is at https://esxi-patches.v-front.de/
 
-$viserver = ""
-$profiledepot = ""
+ param (
+    [Parameter(Mandatory=$true)][string]$viserver,
+    [Parameter(Mandatory=$true)][string]$profile,
+    # depot is typically https://hostupdate.vmware.com/software/VUM/PRODUCTION/main/vmw-depot-index.xml
+    [Parameter(Mandatory=$true)][string]$depot
+    $tools-light
+ )
 
 Import-Module VMware.PowerCLI
-Connect-VIServer $viserver
+Connect-VIServer -Server $viserver
 $esxcli = Get-EsxCli -V2
-$patchargs = $esxcli.software.vib.install.createargs()
-$patchargs.depot = $profiledepot
-$esxcli.software.vib.install.Invoke($patchargs)
-$rebootargs = $esxcli.system.shutdown.reboot.CreateArgs()
-$rebootargs.reason = "Patching"
-$esxcli.system.shutdown.reboot.Invoke($rebootargs)
+
+
+$patchargs = $esxcli.software.profile.update.createargs()
+$patchargs.depot = $depot
+$patchargs.profile = $profile
+$result = $esxcli.software.profile.update.Invoke($patchargs)
+write-output $result
+if ($result) {
+  $rebootargs = $esxcli.system.shutdown.reboot.CreateArgs()
+  $rebootargs.reason = "Patching"
+  $result2 = $esxcli.system.shutdown.reboot.Invoke($rebootargs)
+  write-output $result2
+}
